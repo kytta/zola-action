3abb98c6ac859c4381342da85215eaeb908a3a27
Nikita Karamov <me@kytta.dev>
2023-02-27 01:45:42 +0100

    Add action meta and entrypoint

---
+++ zola.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Check base url
+BASEURL_OPTION=""
+if [ "${INPUT_BASEURL}" != "." ]; then
+    BASEURL_OPTION="--base-url ${INPUT_BASEURL}"
+fi
+
+# Check base url
+DRAFTS_FLAG=""
+if [ "${INPUT_DRAFTS}" = true ]; then
+    DRAFTS_FLAG="--drafts"
+fi
+
+# Build
+eval zola build "${DRAFTS_FLAG}" "${BASEURL_OPTION}" --output-dir "${INPUT_OUTPUT}"
