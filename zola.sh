3f69e7a0bcf9df36ce95e06c0b6c6b0a04ba197a
Nikita Karamov <me@kytta.dev>
2023-02-27 02:46:56 +0100

    Fix envar naming

--- zola.sh
+++ zola.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 # Check base url
 BASEURL_OPTION=""
-if [ "${INPUT_BASE-URL}" != "<NOTSET>" ]; then
-    BASEURL_OPTION="--base-url ${INPUT_BASE-URL}"
+if [ "${INPUT_BASE_URL}" != "<NOTSET>" ]; then
+    BASEURL_OPTION="--base-url ${INPUT_BASE_URL}"
 fi
 
 # Check base url
@@ -15,4 +15,4 @@ if [ "${INPUT_DRAFTS}" = true ]; then
 fi
 
 # Build
-eval zola --root "${INPUT_ROOT}" --config "${INPUT_CONFIG}" build "${BASEURL_OPTION}" --output-dir "${INPUT_OUTPUT-DIR}" "${DRAFTS_OPTION}"
+eval zola --root "${INPUT_ROOT}" --config "${INPUT_CONFIG}" build "${BASEURL_OPTION}" --output-dir "${INPUT_OUTPUT_DIR}" "${DRAFTS_OPTION}"
